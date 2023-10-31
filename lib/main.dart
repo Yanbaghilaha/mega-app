a069125be59bd30da5f23d203f320a071f18db03
Yanbaghilaha <yanbaghilaha@gmail.com>
2023-10-31 13:10:43 +0700

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:e_commerce/screens/home_page.dart';
-// import 'package:e_commerce/screens/login_page/login.dart';
+import 'package:e_commerce/screens/login_page/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Login(),
     );
   }
 }
